664137134e6544d10361816a2a05cfc5871b8c23
Dimitri Hector <joshuahector86@gmail.com>
2023-08-08 18:54:45 -0400

    fixing padding error on the html, sticking navbar and footer to the top and bottom

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,17 @@ import "./Footer.scss";
 function Footer() {
   return (
     <div className="footer">
-      <div>here be the footer</div>
+      <div className="footer-container">
+        <div className="footer-links">
+          <div>LinkedIn</div>
+          <div>Instagram</div>
+          <div>Youtube</div>
+        </div>
+        <div className="copyright">
+          Copyright &copy;
+          {new Date().getFullYear()}&nbsp; Dimitri Hector | All Rights Reserved
+        </div>
+      </div>
     </div>
   );
 }
