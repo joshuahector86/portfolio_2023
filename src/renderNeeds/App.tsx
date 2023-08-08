3451aa04befabbf99b9fe69dd8f5a1ff63b573a6
Dimitri Hector <joshuahector86@gmail.com>
2023-08-08 17:05:16 -0400

    navbar linked up and styled

--- src/renderNeeds/App.tsx
+++ src/renderNeeds/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import "./App.scss";
 import VideoGames from "../Components/VideoGames/VideoGames";
+import Navbar from "../Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
